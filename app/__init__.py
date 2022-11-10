1d60d5033b286eabc363ae7c5a207723ff67a833
Ivan Yeung <iyeung30@stuy.edu>
2022-11-09 19:46:03 -0500

    Work for 11/09/22.

--- app/__init__.py
+++ app/__init__.py
@@ -9,15 +9,13 @@ db = sqlite3.connect(DB_FILE)
 c = db.cursor()
 
 
-from flask import Flask             #facilitate flask webserving
-from flask import render_template   #facilitate jinja templating
-from flask import request           #facilitate form submission
-from flask import session           #facilitate form submission
-from flask import redirect           #facilitate form submission
-from flask import url_for
-
-username = "test"
-password = "1234"
+from flask import *
+# from flask import Flask             #facilitate flask webserving
+# from flask import render_template   #facilitate jinja templating
+# from flask import request           #facilitate form submission
+# from flask import session           #facilitate form submission
+# from flask import redirect           #facilitate form submission
+# from flask import url_for
 
 #the conventional way:
 #from flask import Flask, render_template, request
@@ -41,33 +39,73 @@ def root():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
-    
+
 @app.route("/login/auth", methods=['GET', 'POST'])
 def login_auth():
-    #checks if input for the username form matches with hardcoded username
-    if (request.form["username"] == username):
-        print("***DIAG: username matches ***")
-        #checks if input for the password form matches with hardcoded password
-        if (request.form["password"] == password):
-            print("***DIAG: password matches ***")
-            #when both coditions are met, password is stored in the cookie and the home page is rendered
-            session["username"] = request.form["username"]
-            return redirect("/home", 307)
-        else:
+    form_username = request.form["username"]
+    form_password = request.form["password"]
+
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+
+    c.execute(f"SELECT * FROM users WHERE username = '{form_username}'")
+    users = c.fetchall()
+
+    if len(users) == 1:
+        user = users[0]
+        db_username = user[0]
+        db_password = user[1]
+
+        if (form_username == db_username):
+            print("***DIAG: username matches ***")
+            #checks if input for the password form matches with hardcoded password
+
+            if (form_password == db_password):
+                print("***DIAG: password matches ***")
+                #when both coditions are met, password is stored in the cookie and the home page is rendered
+                session["username"] = form_username
+                return redirect("/home", 307)
+
             print("***DIAG: password did not match ***")
             return redirect("/login", 307)
-    else:
+
         print("***DIAG: username did not match ***")
         return redirect("/login", 307)
+
+    elif len(users) == 0:
+        print("***DIAG: account does not exist ***")
+        return redirect("/login", 307)
+
+    else:
+        return "YOU ARE A FAILURE AT CODING, GET A NEW JOB"
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     return render_template('register.html')
-    
+
 @app.route("/register/auth", methods=['GET', 'POST'])
 def register_auth():
-    db
-    return redirect("/login", 307)
+    username = request.form["username"]
+    password = request.form["password"]
+
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+
+    #checks if inputted username exists
+    c.execute(f"SELECT username FROM users WHERE username = '{username}'")
+    usernames = c.fetchall()
+
+    if len(usernames) == 0:
+        print("***DIAG: username was available***")
+        c.execute(f"INSERT INTO users (username, password) VALUES('{username}', '{password}')")
+
+        c.execute(f"CREATE TABLE {username}(stories TEXT)")
+
+        db.commit()
+        return redirect("/login", 307)
+
+    print("***DIAG: username was not available***")
+    return redirect("/register", 307)
 
 @app.route("/logout", methods=['GET', 'POST'])
 # this function is called by the logout button when it is pressed on the home page
@@ -83,11 +121,17 @@ def home():
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
 
-    c.execute("create table if not exists users(username text, password text)")
-    c.execute("create table if not exists stories(title text, creator text, body text, latest_contribution text)")
+    #table storing usernames and passwords
+    c.execute("CREATE TABLE IF NOT EXISTS users(username TEXT, password TEXT)")
+    #table storing info on stories
+    c.execute("CREATE TABLE IF NOT EXISTS stories(title TEXT, creator TEXT, body TEXT, latest_contribution TEXT)")
+
+    db.commit()
 
     app.debug = True
     app.run()
-    
+
     db.close()
